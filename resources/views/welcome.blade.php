e3bd9430b25877f5916a2d5c9bf52ae0b87d33b7
Jake Bogan <jakebogan01@gmail.com>
2022-05-05 00:42:14 -0400

    updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,24 +14,96 @@
         <link href="{{ asset('css/main.css') }}" rel="stylesheet" />
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="body antialiased h-full">
+    <body class="antialiased h-full">
 
-        <p class="text-5xl text-center mt-14 text-white"> Stay Focused</p>
+{{--        <p class="text-5xl text-center mt-14 text-white"> Stay Focused</p>--}}
 
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+{{--        @if (Route::has('login'))--}}
+{{--            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">--}}
+{{--                @auth--}}
+{{--                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>--}}
+{{--                @else--}}
+{{--                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>--}}
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                    @endif
-                @endauth
+{{--                    @if (Route::has('register'))--}}
+{{--                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>--}}
+{{--                    @endif--}}
+{{--                @endauth--}}
+{{--            </div>--}}
+{{--        @endif--}}
+
+        <div class="bg-white min-h-full flex flex-col lg:relative">
+            <div class="flex-grow flex flex-col">
+                <main class="flex-grow flex flex-col bg-white">
+                    <div class="flex-grow mx-auto max-w-7xl w-full flex flex-col px-4 sm:px-6 lg:px-8">
+                        <div class="flex-shrink-0 pt-10 sm:pt-16">
+                            <a href="/" class="inline-flex">
+                                <span class="sr-only">Workflow</span>
+                                <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="logo">
+                            </a>
+                        </div>
+                        <div class="flex-shrink-0 my-auto py-16 sm:py-32">
+                            <p class="flex items-center text-sm font-semibold text-indigo-600 tracking-wide">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                @if(auth()->user())
+                                    Hello, {{ auth()->user()->name }}
+                                @else
+                                    Hello, visitor
+                                @endif
+                            </p>
+                            <h1 class="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl cursor-default">
+                                <span class="text-blue-400">Stay</span> <span class="text-purple-400">Focused</span>
+                            </h1>
+                            <p class="mt-2 text-purple-600 font-bold">
+                                Welcome, here you can are the creator
+                            </p>
+                            <div class="mt-6 flex space-x-3 sm:border-l sm:border-transparent">
+                                @if (Route::has('login'))
+                                    @auth
+                                        <a href="{{ url('/dashboard') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-bold rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                                            </svg>
+                                            Dashboard
+                                        </a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-bold rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
+                                            </svg>
+                                            Login
+                                        </a>
+
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-bold rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                                                    <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
+                                                </svg>
+                                                Register
+                                            </a>
+                                        @endif
+                                    @endauth
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
-        @endif
+            <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <img class="absolute inset-0 h-full w-full object-cover" src="https://static.wixstatic.com/media/ba0b8d_81aa740dfe3043bfb9ffc3fe8da2e531~mv2.png" alt="creating projects">
+            </div>
+        </div>
 
+        @livewireScripts
     </body>
 </html>
